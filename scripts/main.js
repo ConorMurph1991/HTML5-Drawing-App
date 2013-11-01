2018246841bf69d416d9c1a81e699cf83866a5fd
Conor Murphy <ConorMurph1991@gmail.com>
2013-11-01 14:22:58 +0000

    Setup project structure

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+/* JavaScript Document */
+
+/* Getting the canvas and its context */
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+/* Setting width and height of the canvas */
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;

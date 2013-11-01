eba29ac6e1dfb849b549e4d5ab560a95e19ba636
Conor Murphy <ConorMurph1991@gmail.com>
2013-11-01 18:58:23 +0000

    Created drawing function

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,24 @@
 /* JavaScript Document */
 
-/* Getting the canvas and its context */
+
+/* Variables */
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
+var radius = 10;
 
 /* Setting width and height of the canvas */
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+
+
+/* Function to allow user to draw */
+var draw = function(e) {
+
+	context.beginPath();
+	context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI * 2);
+
+}
+
+canvas.addEventListener("mousedown", draw);
+

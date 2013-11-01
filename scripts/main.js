b4e63051a2d74c42cfb9445751c7ecec63465eea
Conor Murphy <ConorMurph1991@gmail.com>
2013-11-01 19:26:48 +0000

    Updated drawing function to allow use in Firefox

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,48 @@
 /* JavaScript Document */
 
 
-/* Variables */
+/* Canvas Variables */
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
+
+/* Drawing Variables */
 var radius = 10;
+var isMouseMoving = false;
 
 /* Setting width and height of the canvas */
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 
+/* Checks if mouse is down */
+var startDraw = function() {
+
+	isMouseMoving = true;
+
+}
 
 /* Function to allow user to draw */
 var draw = function(e) {
 
-	context.beginPath();
-	context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI * 2);
+	if(isMouseMoving)
+	{
+		context.beginPath();
+		context.arc(e.clientX, e.clientY, radius, 0, Math.PI * 2);
+		context.fill();
+	}
+	
+}
+
+/* Checks if mouse is up */
+var endDraw = function() {
+
+	isMouseMoving = false;
 
 }
 
-canvas.addEventListener("mousedown", draw);
+
+/* Canvas Event Listeners */
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", endDraw);
 
